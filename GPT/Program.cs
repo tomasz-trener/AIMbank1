58ce52a75e5cdc03e3f40eccb10cbf139e335b55
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-22 15:47:59 +0100

    q

---
+++ GPT/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenAI.Audio;
+using OpenAI.Chat;
+using OpenAI.Images;
+using System.ClientModel;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.Json;
+
+namespace Example1
+{
+    //// setx OPENAI_API_KEY ""  // wstaw swój klucz API
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+
+            ChatClient client = new("gpt-4o", Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+
+            ChatCompletion completion = client.CompleteChat("policz 2+2");
+
+            Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+        }
+    }
+}
