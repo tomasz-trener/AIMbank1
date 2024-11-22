334190e3f24df3381258deca29cf948b241581c5
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-11-22 15:33:10 +0100

    q

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,47 +1,20 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using Microsoft.CognitiveServices.Speech;
-using Microsoft.CognitiveServices.Speech.Audio;
-
-
+﻿ 
 namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Klucz i region usługi
-            string subscriptionKey = "klucz..."; // tutaj wstaw swój klucz zasobu
-            string region = "northeurope"; // tutaj wstaw swój region, np. "westeurope"
+            string imie= "Jan";
+            string nazwisko = "Kowalski";
 
-            // Ścieżka do pliku WAV
-            string audioFilePath = @"test_20sek.wav";  // podaj ścieżkę do pliku WAV
-            string outputFilePath = @"transcript.txt"; // ścieżka do pliku wynikowego
-
-            // Konfiguracja Speech SDK
-            var config = SpeechConfig.FromSubscription(subscriptionKey, region);
-            config.SpeechRecognitionLanguage = "pl-PL"; // dla polskiego języka
-
-            // Odczyt pliku WAV
-            using var audioInput = AudioConfig.FromWavFileInput(audioFilePath);
-
-            // Inicjalizacja rozpoznawania mowy
-            using var recognizer = new SpeechRecognizer(config, audioInput);
-
-            // Rozpoczęcie rozpoznawania mowy
-            var result = await recognizer.RecognizeOnceAsync();
-
-            // Sprawdzenie wyników i zapis do pliku
-            if (result.Reason == ResultReason.RecognizedSpeech)
+            //teraz wypisujemy 10 razy imię i nazwisko 
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine($"Transkrypcja: {result.Text}");
-                await File.WriteAllTextAsync(outputFilePath, result.Text);
+                Console.WriteLine(imie + " " + nazwisko);
             }
-            else if (result.Reason == ResultReason.NoMatch)
-            {
-                Console.WriteLine("Nie rozpoznano mowy.");
-            }
+
+
         }
     }
 }
---
+++ Vision/Program.cs
@@ -0,0 +1,86 @@
+﻿using Azure.AI.Vision.Common;
+using Azure.AI.Vision.ImageAnalysis;
+using Azure;
+using System;
+using System.IO;
+
+class ImageAnalysis
+{
+    static void Main()
+    {
+        var serviceOptions = new VisionServiceOptions("adres", new AzureKeyCredential(" "));
+
+        using var imageSource = VisionSource.FromUrl(new Uri("https://idodata.com/wp-content/uploads/2023/12/ComputerOperator-scaled.jpg"));
+
+        var analysisOptions = new ImageAnalysisOptions()
+        {
+            Features = ImageAnalysisFeature.Caption | ImageAnalysisFeature.Text | ImageAnalysisFeature.Tags | ImageAnalysisFeature.Objects | ImageAnalysisFeature.DenseCaptions | ImageAnalysisFeature.People,
+            Language = "en",
+            GenderNeutralCaption = false
+        };
+
+        using var analyzer = new ImageAnalyzer(serviceOptions, imageSource, analysisOptions);
+        var result = analyzer.Analyze();
+
+        if (result.Reason == ImageAnalysisResultReason.Analyzed)
+        {
+            if (result.Caption != null)
+            {
+                Console.WriteLine($"Caption: {result.Caption.Content}");
+            }
+
+            if (result.DenseCaptions != null)
+            {
+                foreach (var varDensecaption in result.DenseCaptions)
+                {
+                    Console.WriteLine($"Dense Caption: {varDensecaption.Content}, {varDensecaption.Confidence}");
+                }
+            }
+
+            if (result.Tags != null)
+            {
+                foreach (var varTag in result.Tags)
+                {
+                    Console.WriteLine($"Tag: {varTag.Name}");
+                }
+            }
+
+            if (result.People != null)
+            {
+                foreach (var varPeople in result.People)
+                {
+                    Console.WriteLine($"People: {varPeople.Confidence}");
+                }
+            }
+
+            if (result.Objects != null)
+            {
+                foreach (var varObject in result.Objects)
+                {
+                    Console.WriteLine($"Object: {varObject.Name}");
+                }
+            }
+
+            if (result.Text != null)
+            {
+                foreach (var varLine in result.Text.Lines)
+                {
+                    Console.WriteLine($"Line: {varLine.Content}");
+                    foreach (var varWord in varLine.Words)
+                    {
+                        Console.WriteLine($"Word: {varWord.Content}");
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            var errorDetails = ImageAnalysisErrorDetails.FromResult(result);
+            Console.WriteLine(" Analysis failed.");
+            Console.WriteLine($"   Error reason : {errorDetails.Reason}");
+            Console.WriteLine($"   Error code : {errorDetails.ErrorCode}");
+            Console.WriteLine($"   Error message: {errorDetails.Message}");
+        }
+    }
+}
